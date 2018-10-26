64e686839ef5cbc51b1a389d41745abbd0c64702
emmeiwhite <emmeiwhite@gmail.com>
2018-10-26 12:28:21 +0530

    First Commit - As props came through, we say Hi to props object

--- src/App.js
+++ src/App.js
@@ -3,23 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    console.log(props);
+  }
   render() {
+    const {message} = this.props; //Interpolation
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <h1>{message}</h1>
       </div>
     );
   }
